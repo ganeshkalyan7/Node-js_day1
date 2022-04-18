dc1de2d9696a8664617717d3a1743c2ddbf9e320
ganeshkalyan7 <ganeshkalyan506@gmail.com>
2022-04-18 18:32:10 +0530

    first commit

--- module.js
+++ module.js
@@ -1,35 +1,37 @@
-const os = require('os');
+
 const fs = require('fs');
 
+const current_time_stamp=new Date()
 
 console.log("this is my project");
 
                  //creating a file
-fs.writeFile('current_date_time.txt' ,"current time stamp", function(err,data){
+fs.writeFile('current_date_time.txt' ,`Current_Time_Stamp : ${current_time_stamp}`, function(err,result){
     if (err){
         
         console.log(err)
     }
-   
-      else{
-        
-        var date=Date()
-          console.log(date);
-       }
 
+        
+        const date=result
 })  
 
     
      
-   
-  
+                      //reading a file
 
-  fs.readFile('current_date_time.txt','utf8',(err,data)=>{
+    fs.readFile('current_date_time.txt','utf8',(err,result)=>{
       if(err){
-          console.log(err)
+        console.log(err)
+        return err
       }
-      else{
-          console.log(data)
-      }
+      const date=result
 
-  })
+      console.log(date)
+
+    })
+    
+
+
+
+ 

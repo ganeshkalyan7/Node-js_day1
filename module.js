b3cd4753c69963a78e05a22f09b44ea5876895d5
ganeshkalyan7 <ganeshkalyan506@gmail.com>
2022-04-18 20:00:10 +0530

    changed to http

--- module.js
+++ module.js
@@ -1,5 +1,6 @@
 
 const fs = require('fs');
+const http = require('http');
 
 const current_time_stamp=new Date()
 
@@ -18,18 +19,32 @@ fs.writeFile('current_date_time.txt' ,`Current_Time_Stamp : ${current_time_stamp
 
     
      
-                      //reading a file
+    //                   //reading a file
 
-    fs.readFile('current_date_time.txt','utf8',(err,result)=>{
-      if(err){
-        console.log(err)
-        return err
-      }
-      const date=result
+    // fs.readFile('current_date_time.txt','utf8',(err,result)=>{
+    //   if(err){
+    //     console.log(err)
+    //     return err
+    //   }
+    //   const date=result
 
-      console.log(date)
+    //   console.log(date)
 
+    // })
+                  // reading a file with http
+    const server=http.createServer((req,res)=>{
+      fs.readFile('current_date_time.txt',(err,data)=>{
+        if(err){
+          console.log(err)
+        }
+        res.write(data);
+        res.end()
+
+
+      })
     })
+    server.listen(4000)
+
     
 
 
